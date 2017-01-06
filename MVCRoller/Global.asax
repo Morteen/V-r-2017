4bbcd289e3088c616baf3ea6b13b8d53bf69f39d
morteen <mortenolsen4@gmail.com>
2017-01-06 14:11:13 +0100

    Add project files.

---
+++ MVCRoller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCRoller.MvcApplication" Language="C#" %>
---
+++ MVCRoller/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCRoller.Startup))]
+namespace MVCRoller
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
